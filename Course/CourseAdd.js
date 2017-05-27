26148b41c886dd7347447c1bd3ee26077c6b9c49
Vid Rajtmajer <vid.rajtmajer@gmail.com>
2017-05-27 22:59:27 +0100

    Removed Teacher Course link

--- Course/CourseAdd.js
+++ Course/CourseAdd.js
@@ -14,28 +14,10 @@ export default (props) => {
         headers: myHeaders,
         body: data
     };
-    var teacherInit = {
-        method: 'POST',
-        headers: myHeaders
-    }
 
     var myRequest = new Request('http://localhost:8080/courses', myInit)
-    var teacherRequest = new Request('http://localhost:8080/teachers/e123/' + course.id, teacherInit)
 
     fetch(myRequest)
-    .then(
-        function(response) {
-            if(response.ok) {
-                return response.blob();
-            }
-            throw new Error('Network response was not ok.');
-        }
-    )
-    .catch(function(err) {
-        console.log('Fetch error :-S', err);
-    })
-
-    fetch(teacherRequest)
     .then(
         function(response) {
             if(response.ok) {
